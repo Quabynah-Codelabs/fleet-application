9c47fd9cd55267b9b10f6ae4e77ac6b1c79e3f1e
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-04-25 08:55:57 +0000

    Removed old templates
    Stored them as a zip file
    Run 'npm install' to get started

--- vue-web-application/src/components/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyA1YVUgV_G-2Mi9G-09o0hL76YLk7qMdU4",
-    authDomain: "fleet-mobile-app.firebaseapp.com",
-    databaseURL: "https://fleet-mobile-app.firebaseio.com",
-    projectId: "fleet-mobile-app",
-    storageBucket: "fleet-mobile-app.appspot.com",
-    messagingSenderId: "242841878369"
-}
