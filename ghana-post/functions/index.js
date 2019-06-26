f1108d1f0a88cb700af4ffac01f0edf594836ed0
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-26 13:05:49 +0000

    Moved all hosting files to respective folder

--- ghana-post/functions/index.js
+++ ghana-post/functions/index.js
@@ -1,8 +1,8 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Test function
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Ghana Post Test transmission!");
+});
