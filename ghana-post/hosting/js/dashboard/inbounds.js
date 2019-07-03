14e7da0c339c5c1c83dbd7fe5ecfb8f9e2b05d5d
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-07-03 18:50:51 +0000

    Added support for inbounds and outbounds

--- ghana-post/hosting/js/dashboard/inbounds.js
+++ ghana-post/hosting/js/dashboard/inbounds.js
@@ -8,12 +8,66 @@ $(document).ready(function() {
     .toString()
     .split(",");
   buildSidebarWithRoles(roles, "inbounds.html");
+
+  // Hide table until content has been loaded completely
+  // toggleTableState(false);
+
+  // load data onto page after a delay.
+  // This is done to resolve the issue of having to load the page before loading the content
   setTimeout(() => {
     loadUserInfo();
     loadTable();
   }, 1200);
 });
 
-const loadTable = () => {
+const toggleTableState = state => {
+  if (state) {
+    $("#main_table").show();
+    $("#empty_container").hide();
+  } else {
+    $("#main_table").hide();
+    $("#empty_container").show();
+  }
+};
 
+const loadTable = () => {
+  // Load table information here
+  $("#dataTable").DataTable();
+  var tableBody = $("data_table_body");
+
+  // TODO: retrieve details for the current office only
+  db.collection("items")
+    // .where("receiving_office", "==", "")
+    .get()
+    .then(snapshots => {
+      if (snapshots.empty) {
+        showNotification("No mail items found. Please try again later");
+      } else {
+        console.log(snapshots);
+        tableBody.empty();
+
+        snapshots.forEach(doc => {
+          var mailItem = doc.data();
+          tableBody.append(`
+          <tr data-href="${doc.id}">
+            <td>${mailItem.key}</td>
+            <td>${mailItem.type.toUpperCase()}</td>
+            <td>${mailItem.sending_office}</td>
+            <td>${mailItem.sending_region}</td>
+            <td>${mailItem.date}</td>
+            <td></td>
+          </tr>
+        `);
+        });
+
+        // Handle click events
+        $(document).on("click", "tr[data-href]", function() {
+          console.log("Showing details for: ", this.dataset.href);
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+      showNotification(err.message);
+    });
 };
--- ghana-post/hosting/js/dashboard/outbounds.js
+++ ghana-post/hosting/js/dashboard/outbounds.js
@@ -8,10 +8,66 @@ $(document).ready(function() {
     .toString()
     .split(",");
   buildSidebarWithRoles(roles, "outbounds.html");
+
+  // Hide table until content has been loaded completely
+  toggleTableState(false);
+
   setTimeout(() => {
     loadUserInfo();
     loadTable();
   }, 1200);
 });
 
-const loadTable = () => {};
+const toggleTableState = state => {
+  if (state) {
+    $("#main_table").show();
+    $("#empty_container").hide();
+  } else {
+    $("#main_table").hide();
+    $("#empty_container").show();
+  }
+};
+
+const loadTable = () => {
+  // Load table information here
+  $("#dataTable").DataTable();
+  var tableBody = $("data_table_body");
+
+  // TODO: retrieve details for the current office only
+  db.collection("items")
+    // .where("sending_office", "==", "")
+    .get()
+    .then(snapshots => {
+      if (snapshots.empty) {
+        // toggleTableState(false);
+        showNotification("No mail items found. Please try again later");
+      } else {
+        console.log(snapshots);
+        tableBody.empty();
+
+        snapshots.forEach(doc => {
+          var mailItem = doc.data();
+          tableBody.append(`
+          <tr data-href="${doc.id}">
+            <td>${mailItem.key}</td>
+            <td>${mailItem.type.toUpperCase()}</td>
+            <td>${mailItem.receiving_office}</td>
+            <td>${mailItem.receiving_region}</td>
+            <td>${mailItem.date}</td>
+            <td></td>
+          </tr>
+        `);
+        });
+
+        // Handle click events
+        $(document).on("click", "tr[data-href]", function() {
+          console.log("Showing details for: ", this.dataset.href);
+          // TODO: show details of the current mail item
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+      showNotification(err.message);
+    });
+};
