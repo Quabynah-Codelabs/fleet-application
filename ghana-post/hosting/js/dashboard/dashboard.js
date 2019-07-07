9efd6bfb86002c49b3f0716a39e18e1afce55a66
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-07-07 15:35:55 +0000

    Update dashboard.js

--- ghana-post/hosting/js/dashboard/dashboard.js
+++ ghana-post/hosting/js/dashboard/dashboard.js
@@ -59,6 +59,7 @@ const loadRolesByLevel = level => {
     case "super":
       console.log("Super admin roles being updated...");
       buildSidebarWithRoles(superAdminRoles, "");
+      addListenersForStats(null);
       $("#username").text("Super Admin");
       $("#user_avatar").attr("src", "./img/default.svg");
       break;
@@ -105,69 +106,135 @@ const addListenersForStats = office => {
   // Get stats collection
   var stats = db.collection("stats");
 
-  // Late
-  stats
-    .where("sending_office", "==", office)
-    .where("type", "==", "late")
-    .onSnapshot(function(querySnapshot) {
-      var records = [];
-      querySnapshot.forEach(function(doc) {
-        records.push(doc.data().key);
+  if (!office) {
+    // Late
+    stats
+      // .where("sending_office", "==", office)
+      .where("type", "==", "late")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
+
+        // Add to page
+        var percentage = records.length;
+        $("#late_count").text(records.length);
+        $("#late_progress").css("width", percentage);
       });
 
-      // Add to page
-      var percentage = records.length;
-      $("#late_count").text(records.length);
-      $("#late_progress").css("width", percentage);
-    });
+    // Inbounds
+    stats
+      // .where("receiving_office", "==", office)
+      .where("type", "==", "inbounds")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
 
-  // Inbounds
-  stats
-    .where("receiving_office", "==", office)
-    .where("type", "==", "inbounds")
-    .onSnapshot(function(querySnapshot) {
-      var records = [];
-      querySnapshot.forEach(function(doc) {
-        records.push(doc.data().key);
+        // Add to page
+        var percentage = records.length;
+        $("#inbounds_count").text(records.length);
+        $("#inbounds_progress").css("width", percentage);
       });
 
-      // Add to page
-      var percentage = records.length;
-      $("#inbounds_count").text(records.length);
-      $("#inbounds_progress").css("width", percentage);
-    });
+    // Outbounds
+    stats
+      // .where("sending_office", "==", office)
+      .where("type", "==", "outbounds")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
 
-  // Outbounds
-  stats
-    .where("sending_office", "==", office)
-    .where("type", "==", "outbounds")
-    .onSnapshot(function(querySnapshot) {
-      var records = [];
-      querySnapshot.forEach(function(doc) {
-        records.push(doc.data().key);
+        // Add to page
+        var percentage = records.length;
+        $("#outbounds_count").text(records.length);
+        $("#outbounds_progress").css("width", percentage);
       });
 
-      // Add to page
-      var percentage = records.length;
-      $("#outbounds_count").text(records.length);
-      $("#outbounds_progress").css("width", percentage);
-    });
+    // OnTime
+    stats
+      // .where("sending_office", "==", office)
+      .where("type", "==", "ontime")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
 
-  // OnTime
-  stats
-    .where("sending_office", "==", office)
-    .where("type", "==", "ontime")
-    .onSnapshot(function(querySnapshot) {
-      var records = [];
-      querySnapshot.forEach(function(doc) {
-        records.push(doc.data().key);
+        // Add to page
+        var percentage = records.length;
+        $("#ontime_count").text(records.length);
+        $("#ontime_progress").css("width", percentage);
+      });
+  } else {
+    // Late
+    stats
+      .where("sending_office", "==", office)
+      .where("type", "==", "late")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
+
+        // Add to page
+        var percentage = records.length;
+        $("#late_count").text(records.length);
+        $("#late_progress").css("width", percentage);
       });
 
-      // Add to page
-      var percentage = records.length;
-      $("#ontime_count").text(records.length);
-      $("#ontime_progress").css("width", percentage);
-    });
+    // Inbounds
+    stats
+      .where("receiving_office", "==", office)
+      .where("type", "==", "inbounds")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
+
+        // Add to page
+        var percentage = records.length;
+        $("#inbounds_count").text(records.length);
+        $("#inbounds_progress").css("width", percentage);
+      });
+
+    // Outbounds
+    stats
+      .where("sending_office", "==", office)
+      .where("type", "==", "outbounds")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
+
+        // Add to page
+        var percentage = records.length;
+        $("#outbounds_count").text(records.length);
+        $("#outbounds_progress").css("width", percentage);
+      });
+
+    // OnTime
+    stats
+      .where("sending_office", "==", office)
+      .where("type", "==", "ontime")
+      .onSnapshot(function(querySnapshot) {
+        var records = [];
+        querySnapshot.forEach(function(doc) {
+          records.push(doc.data().key);
+        });
+
+        // Add to page
+        var percentage = records.length;
+        $("#ontime_count").text(records.length);
+        $("#ontime_progress").css("width", percentage);
+      });
+  }
 
   // Starts showing stats
   initCharts();
